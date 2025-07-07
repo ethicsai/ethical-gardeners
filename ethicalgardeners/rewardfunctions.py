f4e20ec7ac6b87b71c1dc754e0400e593ae2edcb
Enzo Dos Anjos <43908457+enzo-dosanjos@users.noreply.github.com>
2025-07-07 11:36:48 +0200

    Add RewardFunctions class
    Implement 3 reward functions and a method to combine the 3
    into a mono-objective reward for pettingzoo.
    
    Features:
    - An ecologial reward following planting and harvesting
    based on the impact on the pollution.
    - A well-being reward based on the earnings of
    the agent.
    - A  biodiversity reward based on the diversity of planted
    flowers.

---
+++ ethicalgardeners/rewardfunctions.py
@@ -0,0 +1,289 @@
+"""
+Module containing reward functions for the Ethical Gardeners environment.
+
+This module defines some reward function used to compute rewards for agents:
+- `ComputeEcologyReward`: Computes the ecological reward based on the agent's
+    action, specifically for planting and harvesting flowers.
+- `ComputeWellbeingReward`: Computes the well-being reward based on the agent's
+    action, specifically for selling flowers and give a penalty for not
+    earning money.
+- `ComputeBiodiversityReward`: Computes the biodiversity reward based on the
+    number of different flower types planted by all the agents and how much
+    the agents helps increase diversity.
+"""
+from math import log
+
+from ethicalgardeners.action import Action
+from ethicalgardeners.constants import MAX_PENALTY_TURNS
+
+
+class RewardFunctions:
+    """
+    Class for computing rewards in the Ethical Gardeners environment.
+
+    This class is responsible for calculating different types of rewards for
+    agents based on their actions in the environment. The rewards are designed
+    to encourage ecologically beneficial behaviors, well-being, and
+    biodiversity.
+
+    Each reward component is normalized to a range between -1 and 1.
+    """
+
+    def __init__(self):
+        """Create the RewardFunctions object."""
+        pass
+
+    def computeReward(self, grid_world_prev, grid_world, agent, action):
+        """
+        Compute the mono-objective reward for an agent based on its action in
+        the environment.
+
+        The reward is a combination of ecological, well-being, and biodiversity
+        rewards, normalized to a range between -1 and 1.
+
+        Args:
+            grid_world_prev (:class:`~ethicalgardeners.worldgrid.GridWorld`):
+                The grid world environment before the action.
+            grid_world (:class:`~ethicalgardeners.worldgrid.GridWorld`): The
+                grid world environment.
+            agent (:class:`~ethicalgardeners.agent.Agent`): The agent
+                performing the action.
+            action (:class:`~ethicalgardeners.worldgrid.Action`): The action
+                performed.
+
+        Returns:
+            dict: A dictionary containing the ecological, well-being, and
+            biodiversity rewards, as well as the total reward averaged across
+            these components.
+        """
+        ecology_reward = self.computeEcologyReward(grid_world_prev, grid_world,
+                                                   agent, action)
+        wellbeing_reward = self.computeWellbeingReward(grid_world_prev,
+                                                       grid_world, agent,
+                                                       action)
+        biodiversity_reward = self.computeBiodiversityReward(grid_world_prev,
+                                                        grid_world, agent,
+                                                        action)
+
+        return {'ecology': ecology_reward, 'wellbeing': wellbeing_reward,
+                'biodiversity': biodiversity_reward, 'total': (
+                ecology_reward + wellbeing_reward + biodiversity_reward) / 3}
+
+    def computeEcologyReward(self, grid_world_prev, grid_world, agent, action):
+        """
+        Compute the ecological reward for an agent based on its action in the
+        environment.
+
+        For planting actions, calculates the expected future impact of
+        pollution reduction, normalized against the maximum theoretical impact.
+        For harvesting actions, multiply the impact the flower had on the
+        environment before harvesting with the pollution of the cell, also
+        normalized against the maximum. Penalizes harvesting actions only if
+        the pollution level is above the minimum pollution level.
+
+
+        Args:
+            grid_world_prev (:class:`~ethicalgardeners.worldgrid.GridWorld`):
+                The grid world environment before the action.
+            grid_world (:class:`~ethicalgardeners.worldgrid.GridWorld`): The
+                grid world environment.
+            agent (:class:`~ethicalgardeners.agent.Agent`): The agent
+                performing the action.
+            action (:class:`~ethicalgardeners.worldgrid.Action`): The action
+                performed.
+
+        Returns:
+            float: The normalized ecological reward (between -1 and 1) for
+            planting and harvesting actions, or 0 for other actions.
+        """
+        # Reward computed only for planting and harvesting actions
+        if action == Action.PLANT:
+            p_max = grid_world.max_pollution
+            p_min = grid_world.min_pollution
+            position = agent.position
+            cell = grid_world.get_cell(position)
+
+            # Check if a flower has been planted in the cell
+            if not cell.has_flower():
+                return 0.0
+
+            flower = cell.flower
+            flower_type = flower.flower_type
+            flower_pollution_reduction = (
+                grid_world.flower_data[flower_type].pollution_reduction)
+
+            # Current pollution level in the cell
+            cell_pollution = cell.pollution
+
+            # Compute the maximum possible impact
+            r_max = (p_max - p_min) * 1/0.01
+
+            # Compute the expected future impact of pollution reduction
+            r_plant = (sum(flower_pollution_reduction) * 1 / (
+                        cell_pollution - p_max + 0.01))  # Avoid zero-division
+
+            # Normalize the reward against the maximum possible impact
+            if r_max > 0:
+                return r_plant / r_max
+            else:
+                return 0.0
+        elif action == Action.HARVEST:
+            p_max = grid_world.max_pollution
+            p_min = grid_world.min_pollution
+            position = agent.position
+            prev_cell = grid_world_prev.get_cell(position)
+            cell = grid_world.get_cell(position)
+
+            # Check if a flower has been harvested in the cell
+            if cell.has_flower():
+                return 0.0
+
+            flower = prev_cell.flower
+            flower_type = flower.flower_type
+            if len(grid_world.flower_data[
+                       flower_type].pollution_reduction) == 0:
+                return 0.0
+            flower_pollution_grown_reduction = (
+                grid_world.flower_data[flower_type].pollution_reduction[-1])
+
+            # Current pollution level in the cell
+            cell_pollution = cell.pollution
+
+            # Compute the maximum possible impact
+            r_max = p_max - p_min
+
+            # Compute the expected future impact of pollution reduction
+            r_harvest = (flower_pollution_grown_reduction *
+                         (cell_pollution - p_min))
+
+            # Normalize the reward against the maximum possible impact
+            if r_max > 0:
+                return r_harvest / r_max
+            else:
+                return 0.0
+        else:
+            return 0.0
+
+    def computeWellbeingReward(self, grid_world_prev, grid_world, agent,
+                               action):
+        """
+        Compute the well-being reward for an agent based on its action in the
+        environment.
+
+        Well-being rewards are calculated based on the price of the harvested
+        flowers compared to the most expensive flower type. Penalises the agent
+        for not earning money by giving a penalty based on the number of turns
+        without income, normalized to a maximum penalty.
+
+        Args:
+            grid_world_prev (:class:`~ethicalgardeners.worldgrid.GridWorld`):
+                The grid world environment before the action.
+            grid_world (:class:`~ethicalgardeners.worldgrid.GridWorld`): The
+                grid world environment.
+            agent (:class:`~ethicalgardeners.agent.Agent`): The agent
+                performing the action.
+            action (:class:`~ethicalgardeners.worldgrid.Action`): The action
+                performed.
+        Returns:
+            float: The normalized well-being reward (between -1 and 1).
+        """
+        # Reward computed only for harvesting actions
+        if action == Action.HARVEST:
+            position = agent.position
+            prev_cell = grid_world_prev.get_cell(position)
+            cell = grid_world.get_cell(position)
+
+            # Check if a flower has been harvested in the cell
+            if cell.has_flower():
+                return 0.0
+
+            flower = prev_cell.flower
+            flower_type = flower.flower_type
+
+            # Get the monetary value of the flower
+            flower_value = grid_world.flower_data[flower_type]['price']
+
+            # Normalize the reward based on the maximum possible value
+            highest_flower_value = max(
+                grid_world.flower_data[ft]['price'] for ft in
+                grid_world.flower_data)
+            return flower_value / highest_flower_value
+        else:
+            # Calculate penalty for not earning money
+            return -min(agent.turns_without_income / MAX_PENALTY_TURNS, 1.0)
+
+    def computeBiodiversityReward(self, prev_grid_world, grid_world, agent,
+                                  action):
+        """
+        Compute the biodiversity reward for an agent based on its action in the
+        environment.
+
+        Biodiversity rewards are calculated based on the number of different
+        flower types planted by the agent using the Shannon-Wiener index.
+        Compares the index before and after the planting action to determine
+        the impact.
+
+        Args:
+            prev_grid_world (:class:`~ethicalgardeners.worldgrid.GridWorld`):
+                The grid world environment before the action.
+            grid_world (:class:`~ethicalgardeners.worldgrid.GridWorld`): The
+                grid world environment.
+            agent (:class:`~ethicalgardeners.agent.Agent`): The agent
+                performing the action.
+            action (:class:`~ethicalgardeners.worldgrid.Action`): The action
+                performed.
+
+        Returns:
+            float: The normalized biodiversity reward (between -1 and 1).
+        """
+        if action != Action.PLANT:
+            return 0.0
+
+        position = agent.position
+        cell = grid_world.get_cell(position)
+
+        # Check if a flower has been planted in the cell
+        if not cell.has_flower():
+            return 0.0
+
+        # Get the flower type that has been planted
+        planted_flower_type = cell.flower.flower_type
+
+        # Count the number of different flower types planted by all agents
+        flowers = {flower_type: 0 for flower_type in
+                   grid_world.flower_data.keys()}
+        total_flowers = 0
+
+        for agent in grid_world.agents:
+            for flower_type, flower_list in agent.flowers_planted.items():
+                flowers[flower_type] += len(flower_list)
+                total_flowers += len(flower_list)
+
+        # Create a dictionary to hold the flower counts before planting
+        prev_flowers = dict(flowers)  # Copy current flower counts
+        prev_flowers[planted_flower_type] -= 1  # Remove the planted flower
+        prev_total = total_flowers - 1
+
+        # Compute the ratio of each flower type and use Shannon-Wiener index
+        # to compute biodiversity
+        # before
+        prev_biodiversity = 0
+        for flower_type in prev_flowers:
+            if prev_flowers[flower_type] > 0:
+                ratio = prev_flowers[flower_type] / prev_total
+                prev_biodiversity -= ratio * log(ratio)
+
+        # after
+        biodiversity = 0
+        for flower_type in flowers:
+            if flowers[flower_type] > 0:
+                ratio = flowers[flower_type] / total_flowers
+                biodiversity -= ratio * log(ratio)
+
+        max_biodiversity = log(len(grid_world.flower_data))
+
+        # Compute the impact of the agent's planting action and normalize
+        if max_biodiversity > 0:
+            return (biodiversity - prev_biodiversity) / max_biodiversity
+        else:
+            return 0.0
